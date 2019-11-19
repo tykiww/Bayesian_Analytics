c0eb3082f3472b59fe8cf18f9bd9914c72223435
Tyki Wada <36966666+tykiww@users.noreply.github.com>
2019-11-19 14:28:35 -0700

    Add files via upload

---
+++ Logistic_Regression/bondjag.R
@@ -0,0 +1,32 @@
+library(R2jags)
+mdl <- "
+model  {
+  for (i in 1:21){
+  pressure[i] ~ dnorm(mu[i],1/s2error)
+  mu[i] <- gamma[metn[i]]  + u[ingot[i]]
+}
+for (i in 1:3){
+  gamma[i] ~ dnorm(75,.001)
+}
+for (i in 1:7){
+  u[i] ~ dnorm(0,1/s2ing)
+}
+s2error ~ dgamma(1.5,.1)
+s2ing ~ dgamma(1.5,.1)
+}
+"
+
+writeLines(mdl,'mixedmods.txt')
+
+metn <- bond[,4]
+ingot <- bond[,1]
+pressure <- bond[,3]
+
+data.jags <- c('metn','ingot','pressure')
+parms <- c('gamma','s2error','s2ing')
+
+
+mixedmods.sim <- jags(data=data.jags,inits=NULL,
+     parameters.to.save=parms,
+	   model.file='mixedmods.txt',n.iter=12000,
+     n.burnin=2000,n.chains=5,n.thin=5)
---
+++ Logistic_Regression/moisturejag.R
@@ -0,0 +1,91 @@
+library(R2jags)
+mdl <- '
+  model {
+    for (i in 1:60){
+      yield[i] ~ dnorm(mu[i],1/vv)
+      mu[i] <- b0 + b1*moisture[i]
+    }
+    b0 ~ dnorm(30,.00001)
+    b1 ~ dnorm(0,.00001)
+    vv ~ dgamma(2,.25)
+  } 
+'
+writeLines(mdl,'linreg.txt')
+yield <- wheat$yield
+moisture <- wheat$moisture
+data.jags <- c('yield','moisture')
+parms <- c('b0','b1','vv')
+linreg.sim <- jags(data = data.jags,parameters.to.save = parms,
+                   model.file = 'linreg.txt',
+                   inits = NULL, n.iter = 12000, n.burnin = 2000, 
+                   n.thin=1,n.chains=1)
+
+mdl <- '
+  model {
+for (i in 1:60){
+yield[i] ~ dnorm(mu[i],1/vv)
+mu[i] <- b0[variety[i]] + b1*moisture[i]
+}
+for (i in 1:10){
+b0[i] ~ dnorm(30,.00001) }
+b1 ~ dnorm(0,.00001)
+vv ~ dgamma(2,.25)
+} 
+'
+writeLines(mdl,'randint.txt')
+yield <- wheat$yield
+moisture <- wheat$moisture
+variety <- wheat$variety
+data.jags <- c('yield','moisture','variety')
+parms <- c('b0','b1','vv')
+randint.sim <- jags(data = data.jags,parameters.to.save = parms,model.file = 'randint.txt',
+                   inits = NULL, n.iter = 12000, n.burnin = 2000, n.thin=1,n.chains=1)
+
+mdl <- '
+  model {
+for (i in 1:60){
+yield[i] ~ dnorm(mu[i],1/vv)
+mu[i] <- b0[variety[i]] + b1*moisture[i]
+}
+for (i in 1:10){
+b0[i] ~ dnorm(mub0,1/vvint) }
+mub0 ~ dnorm(30,.000001)
+vvint ~ dgamma(4,.25)
+b1 ~ dnorm(0,.00001)
+vv ~ dgamma(2,.25)
+} 
+'
+writeLines(mdl,'hierint.txt')
+yield <- wheat$yield
+moisture <- wheat$moisture
+variety <- wheat$variety
+data.jags <- c('yield','moisture','variety')
+parms <- c('b0','b1','vv','mub0','vvint')
+hierint.sim <- jags(data = data.jags,parameters.to.save = parms,model.file = 'hierint.txt',
+                    inits = NULL, n.iter = 12000, n.burnin = 2000, n.thin=1,n.chains=1)
+
+mdl <- '
+  model {
+for (i in 1:60){
+yield[i] ~ dnorm(mu[i],1/vv)
+mu[i] <- b0[variety[i]] + b1[variety[i]]*moisture[i]
+}
+for (i in 1:10){
+b0[i] ~ dnorm(mub0,1/vvint)
+b1[i] ~dnorm(mub1,1/vvslp)
+}
+mub0 ~ dnorm(30,.000001)
+vvint ~ dgamma(4,.25)
+mub1 ~ dnorm(0,.00001)
+vvslp ~ dgamma(1.1,.5)
+vv ~ dgamma(1.1,.5)
+} 
+'
+writeLines(mdl,'hier.txt')
+yield <- wheat$yield
+moisture <- wheat$moisture
+variety <- wheat$variety
+data.jags <- c('yield','moisture','variety')
+parms <- c('b0','b1','vv','mub0','vvint','mub1','vvslp')
+hier.sim <- jags(data = data.jags,parameters.to.save = parms,model.file = 'hier.txt',
+                    inits = NULL, n.iter = 12000, n.burnin = 2000, n.thin=1,n.chains=1)
